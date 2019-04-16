33c93575f56beca230ad286ce337e20ec1ae5b7e
Nathan Howard <nathanhaashoward@gmail.com>
2019-04-16 17:52:56 -0600

    Add files via upload

--- resources/js/game.js
+++ resources/js/game.js
@@ -2,48 +2,58 @@ var keyPressed = false;
 var startTime = new Date();
 var currentKey = "a";
 var pressedKey = "";
-var interval = 1000;
+var interval = 2000;
 var running = false;
 var keys = ['a', 's', 'd', 'f'];
 var si;
+var audio;
+var ready = true;
 
 
 $(document).ready(function() {
+	audio = document.getElementById("audio");
+	audio.load();
 	$("#play").click(function() {
-		document.getElementById("audio").play();
+		audio.play();
 		$("#output").text("Ready...");
-		si = setInterval(updateKey, 4000);
+		setTimeout(function(){si = setInterval(updateKey, 2000);}, 2200);
 	});
 });
 
 function updateKey() {
-	$("#game").attr("src", "../resources/img/s.jpg");
-	startTime = new Date();
-	currentKey = keys[Math.floor(Math.random() * 4)];
-	$("#output").text("Press " + currentKey);
-	running = true;
+	if(ready) {
+		$("#game").attr("src", "../resources/img/s.jpg");
+		startTime = new Date();
+		currentKey = keys[Math.floor(Math.random() * 4)];
+		$("#output").text("Press " + currentKey);
+		running = true;
+		ready = false;
+	} else {
+		audio.pause();
+		audio.load();
+		$("#output").text("Too slow! Game Over!");
+		$("#game").attr("src", "../resources/img/explosion.jpg");
+		running = false;
+		clearInterval(si);
+	}
 }
 
 function main() {
 	if(running) {
 		var now = new Date();
-		var ms = (now.getSeconds() * 2000 + now.getMilliseconds()) - (startTime.getSeconds() * 1000 + startTime.getMilliseconds());
-		if(ms > interval) {
-			$("#output").text("Too slow! Game Over!");
-			$("#game").attr("src", "../resources/img/explosion.jpg");
+		var ms = (now.getSeconds() * 1000 + now.getMilliseconds()) - (startTime.getSeconds() * 1000 + startTime.getMilliseconds());
+		if(pressedKey.length != 0) {
 			running = false;
-			clearInterval(si);
-		} else {
-			if(pressedKey.length != 0) {
-				running = false;
-				if(pressedKey == currentKey) {
-					$("#output").text("Good job.");
-					$("#game").attr("src", "../resources/img/check.jpg");
-				} else {
-					$("#game").attr("src", "../resources/img/explosion.jpg");
-					$("#output").text("Wrong Key! Game Over!");
-					clearInterval(si);
-				}
+			if(pressedKey == currentKey) {
+				$("#output").text("Good job.");
+				$("#game").attr("src", "../resources/img/check.jpg");
+				ready = true;
+			} else {
+				audio.pause();
+				audio.load();
+				$("#game").attr("src", "../resources/img/explosion.jpg");
+				$("#output").text("Wrong Key! Game Over!");
+				clearInterval(si);
 			}
 		}
 	}
