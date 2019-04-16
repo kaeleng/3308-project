41c7bff54ac11268138234c5f6d4caa2b8dc91f8
kaeleng <kagu1593@colorado.edu>
2019-04-16 05:49:32 -0600

    login check

--- server.js
+++ server.js
@@ -74,10 +74,55 @@ app.get('/game', function(req, res) {
 
 
 app.get('/login', function(req, res) {
-	res.render('pages/login.ejs',{
-		local_css:"signin.css", 
-		my_title:"Login Page"
-	});
+  var query = 'select * from users';
+  db.any(query)
+      .then(function(rows) {
+          res.render('pages/login.ejs',{
+            my_title: "Login Page",
+            data: rows
+          })
+      })
+      .catch(function (err) {
+            // display error message in case an error
+            //request.flash('error', err);
+      console.log("Not Working");
+            res.render('pages/login.ejs', {
+                my_title: 'Login Page',
+                data: ''
+            })
+        })
+});
+ 
+
+
+app.get('/login/check', function(req, res) {
+  var users_name = req.query.username;
+  var users_password = req.query.password;
+  var user_info =  "select username, password from users where username = '" + users_name + "' and password = '" + users_password+"';";
+    // db.query("select * from users", function(err, result, fields) {
+    //   console.log("Here");
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   console.log(result)
+    // });
+  db.any(user_info)
+      .then(function(rows) {
+          res.render('pages/login.ejs',{
+            my_title: "Login Page",
+            data: rows
+
+          })
+      })
+    .catch(function (err) {
+            // display error message in case an error
+            //request.flash('error', err);
+      console.log("Not Working");
+            res.render('pages/login.ejs', {
+                my_title: 'Login Page',
+                data: ''
+            })
+        })
 });
 
 // registration page 
